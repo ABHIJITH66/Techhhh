f9385217c3c02d9c26cce9bef3e2db9156f27e92
Glitch (tech-mod) <none>
2021-04-30 08:00:58 +0000

    🏨🐇 Checkpoint ./commands/fun/snipe.js:29885963/357

--- commands/fun/snipe.js
+++ commands/fun/snipe.js
@@ -4,3 +4,14 @@ module.exports = {
   name: "snipe",
   aliases: [],
   category: "Fun",
+  description: "A snipe command",
+  run:async (client, message, args) => {
+    message.delete();
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(msg.author)
+    .colour: "
+    .setdescription(msg.content);
+    if (msg.image) embed.set.Image(msg.image);
+   return  message.channel.send(embed);
+    }
+  };
