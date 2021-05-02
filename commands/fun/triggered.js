cb77776751af6249ef2ee68e24680dc71b7a32a4
Glitch (techmod-o) <none>
2021-05-02 14:17:47 +0000

    👦🚂 Checkpoint ./commands/fun/wasted.js:29885963/1110 ./commands/fun/triggered.js:29885963/575 ./commands/fun/play.js:29885963/655 ./commands/fun/search.js:29885963/635

--- commands/fun/triggered.js
+++ commands/fun/triggered.js
@@ -2,15 +2,15 @@ const  Random = require("srod-v2");
 const Discord = require("discord.js");
 
 module.exports = {
-  name: "wasted",
-  aliases: ["wtd"],
+  name: "triggered",
+  aliases: ["tgd"],
   category: "Image",
-  description: "Return A Wasted Image!",
-  usage: "Wasted | <Mention Or ID>",
+  description: "Return A Triggered Image!",
+  usage: "Triggered | <Mention Or ID>",
   run: async (client, message, args) => {
     
     const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-    const Data = await Random.Wasted({ Image: Member.user.displayAvatarURL({ format: "png" }), Color: "RANDOM" });
+    const Data = await Random.Triggered({ Image: Member.user.displayAvatarURL({ format: "jpg" }), Color: "RANDOM" });
 
     return message.channel.send(Data);
   }
