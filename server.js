8f7d15851e0ac410c226dbcbfd451bbac3e22a48
Glitch (techmod-o) <none>
2021-05-02 15:34:52 +0000

    👛🍕 Checkpoint ./package.json:29885963/11613 ./server.js:29885963/308

--- server.js
+++ server.js
@@ -49,17 +49,6 @@ module.exports.help = {
   name: "channel"
 }
 client.on("ready", async () => { console.log(`ready!`); 
-      const cooldowns = new Collection();
-const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-
-/**
- * Client Events
- */
-client.on("ready", () => {
-   function randomStatus() {
- let status = ["TECH TIPS BY ABHIJITH", "=help"]
-let rstatus = Math.floor(Math.random() * status.length);
-//hi
 
 //
 client.on("message", (message) =>{
