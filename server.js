e191c36fe4da694f59a668e285c9bbbe254f2779
Glitch (techmod-o) <none>
2021-05-06 07:52:01 +0000

    🎖🌟 Checkpoint ./server.js:29885963/11326

--- server.js
+++ server.js
@@ -5,69 +5,68 @@ const { Prefix, Token, Color } = require("./config.js");
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.db = require("quick.db");
-const moment = require("moment")
-const db = require("quick.db")
-var jimp = require('jimp');
+const moment = require("moment");
+const db = require("quick.db");
+var jimp = require("jimp");
 const canvas = require("discord-canvas");
 
 //-----database-------ap
 const { Default_Prefix, Support, Owner, WelcomeImage, LeaveImage } = require("./config.js");
 
 module.exports.run = async (bot, message, args) => {
+  let permission = message.member.hasPermission("ADMINISTRATOR");
 
-	let permission = message.member.hasPermission("ADMINISTRATOR");
+  if (!permission) return message.channel.send("You are missing the permission `ADMINISTRATOR`");
 
-if(!permission) return message.channel.send("You are missing the permission `ADMINISTRATOR`")
+  let cArgs = args[0];
 
- let cArgs = args[0]
- 
- if(isNaN(cArgs)) return message.channel.send("You must specify a valid id for the welcome channel!")
-	 
- try{
-	 bot.guilds.get(message.guild.id).channels.get(cArgs).send("Welcome channel set!")
-	 
- db.set(`${message.guild.id}`, cArgs)
- 
- message.channel.send("You have successfully set the welcome channel to <#" + cArgs + ">")
-return;
- }catch(e){
-	return message.channel.send("Error: missing permissions or channel doesn't exist")
- }
- 
- 
-}
+  if (isNaN(cArgs)) return message.channel.send("You must specify a valid id for the welcome channel!");
+
+  try {
+    bot.guilds
+      .get(message.guild.id)
+      .channels.get(cArgs)
+      .send("Welcome channel set!");
+
+    db.set(`${message.guild.id}`, cArgs);
+
+    message.channel.send("You have successfully set the welcome channel to <#" + cArgs + ">");
+    return;
+  } catch (e) {
+    return message.channel.send("Error: missing permissions or channel doesn't exist");
+  }
+};
 module.exports.help = {
   name: "channel"
-}
-client.on("ready", async () => { console.log(`ready!`); client.user .setActivity(`Use =help For Help`, { type: "Playing" }) .catch(error => console.log(error)); });
+};
+client.on("ready", async () => {
+  console.log(`ready!`);
+  client.user.setActivity(`Use =help For Help`, { type: "Playing" }).catch(error => console.log(error));
+});
 //hi
 
+//command
 
-//command 
-
-
-
-let modules = ["fun",'info', "moderation"];
+let modules = ["fun", "info", "moderation"];
 modules.forEach(function(module) {
-fs.readdir(`./commands/${module}`, (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(function(file) {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${module}/${file}`);
-    let commandName = file.split(".")[0];
-    client.commands.set(commandName, props);
-    console.log("Loading Command: "+commandName)
+  fs.readdir(`./commands/${module}`, (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(function(file) {
+      if (!file.endsWith(".js")) return;
+      let props = require(`./commands/${module}/${file}`);
+      let commandName = file.split(".")[0];
+      client.commands.set(commandName, props);
+      console.log("Loading Command: " + commandName);
+    });
   });
 });
-});
 //-
-  
+
 client.on("message", async message => {
   if (message.channel.type === "dm") return;
   if (message.author.bot) return;
   if (!message.guild) return;
-  if (!message.member)
-    message.member = await message.guild.fetchMember(message);
+  if (!message.member) message.member = await message.guild.fetchMember(message);
 
   if (!message.content.startsWith(Prefix)) return;
 
@@ -79,8 +78,7 @@ client.on("message", async message => {
 
   if (cmd.length === 0) return;
 
-  let command =
-    client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
+  let command = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
 
   if (!command) return;
 
@@ -95,27 +93,31 @@ client.on("message", async message => {
     `User : ${message.author.tag} (${message.author.id}) Server : ${message.guild.name} (${message.guild.id}) Command : ${command.name}`
   );
 });
- //welcome 
-	client.on("guildMemberAdd", async member => {
+//welcome
+client.on("guildMemberAdd", async member => {
   let Channel = await db.fetch(`Welcome_${member.guild.id}_Channel`);
   if (!Channel) return;
   let Message = await db.fetch(`Welcome_${member.guild.id}_Msg`);
   if (!Message) Message = `Welcome To The Server!`;
-  
+
   if (member.user.username.length > 25) member.user.username = member.user.username.slice(0, 25) + "...";
   if (member.guild.name.length > 15) member.guild.name = member.guild.name.slice(0, 15) + "...";
-  
-  let Msg = Message.toLowerCase().replace("<servername>", member.guild.name).replace("<membername>", member.user.username).replace("<membermention>", `<@${member.user.id}>`);
+
+  let Msg = Message.toLowerCase()
+    .replace("<servername>", member.guild.name)
+    .replace("<membername>", member.user.username)
+    .replace("<membermention>", `<@${member.user.id}>`);
   let Welcomed = new canvas.Welcome();
-  let Image = await Welcomed
-  .setUsername(member.user.username)
-  .setDiscriminator(member.user.discriminator)
-  .setGuildName(member.guild.name)
-  .setAvatar(member.user.displayAvatarURL({ dynamic: false, format: "jpg" }))
-  .setMemberCount(member.guild.memberCount)
-  .setBackground(WelcomeImage || "https://images.wallpaperscraft.com/image/landscape_art_road_127350_1280x720.jpg")
-  .toAttachment();
-  
+  let Image = await Welcomed.setUsername(member.user.username)
+    .setDiscriminator(member.user.discriminator)
+    .setGuildName(member.guild.name)
+    .setAvatar(member.user.displayAvatarURL({ dynamic: false, format: "jpg" }))
+    .setMemberCount(member.guild.memberCount)
+    .setBackground(
+      WelcomeImage || "https://images.wallpaperscraft.com/image/landscape_art_road_127350_1280x720.jpg"
+    )
+    .toAttachment();
+
   let Attachment = new Discord.MessageAttachment(Image.toBuffer(), "Welcome.png");
   return client.channels.cache.get(Channel).send(Msg, Attachment);
 });
@@ -125,32 +127,35 @@ client.on("guildMemberRemove", async member => {
   if (!Channel) return;
   let Message = await db.fetch(`Leave_${member.guild.id}_Msg`);
   if (!Message) Message = `${member.user.username} Has Left The Server!`;
-  
+
   if (member.user.username.length > 25) member.user.username = member.user.username.slice(0, 25) + "...";
   if (member.guild.name.length > 15) member.guild.name = member.guild.name.slice(0, 15) + "...";
-  
-  let Msg = Message.toLowerCase().replace("<servername>", member.guild.name).replace("<membername>", member.user.username).replace("<membermention>", `<@${member.user.id}>`);
+
+  let Msg = Message.toLowerCase()
+    .replace("<servername>", member.guild.name)
+    .replace("<membername>", member.user.username)
+    .replace("<membermention>", `<@${member.user.id}>`);
   let Leaved = new canvas.Goodbye();
-  let Image = await Leaved
-  .setUsername(member.user.username)
-  .setDiscriminator(member.user.discriminator)
-  .setGuildName(member.guild.name)
-  .setAvatar(member.user.displayAvatarURL({ dynamic: false, format: "jpg" }))
-  .setMemberCount(member.guild.memberCount)
-  .setBackground(LeaveImage || "https://images.wallpaperscraft.com/image/cat_night_lights_74375_1280x720.jpg")
-  .toAttachment();
-  
+  let Image = await Leaved.setUsername(member.user.username)
+    .setDiscriminator(member.user.discriminator)
+    .setGuildName(member.guild.name)
+    .setAvatar(member.user.displayAvatarURL({ dynamic: false, format: "jpg" }))
+    .setMemberCount(member.guild.memberCount)
+    .setBackground(
+      LeaveImage || "https://images.wallpaperscraft.com/image/cat_night_lights_74375_1280x720.jpg"
+    )
+    .toAttachment();
+
   let Attachment = new Discord.MessageAttachment(Image.toBuffer(), "Welcome.png");
   return client.channels.cache.get(Channel).send(Msg, Attachment);
 });
 //end welcome
 
-//c 
+//c
 // Default
 
 // Custom
 let SnakeGame = require("discord-snakegame");
 SnakeGame = new SnakeGame({ backg: "🟫", snake: "🟠", snakeTail: "🟡", apple: "🍎" });
-
 
 client.login(Token);
